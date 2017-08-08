023840d8b71ab7bbcf4df4205673e538adbb5c2e
205-20 <205-20@205-20-PC>
2017-08-08 17:06:07 +0900

    main css 수정,삭제

--- youngeoun.Tis.0807/WebContent/res/mypage/js/index.js
+++
@@ -1,9 +0,0 @@
-var popupid = $('#login');
-$("#launch").click(function() {
-  $('.overlay').show();
-  popupid.show();
-});
-$("#closepopup").click(function() {
-  $('.overlay').hide();
-  popupid.hide();
-});
