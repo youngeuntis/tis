72625f31dd30bae0e28e5f9c0bd28ce7e56d0837
205_5 <205_5@205_5-PC>
2017-08-11 14:31:34 +0900

    백진우 작업 중

---
+++ youngeoun.Tis.0807/src/youngun/tis/travel/blog/Blog.java
@@ -0,0 +1,24 @@
+package youngun.tis.travel.blog;
+
+public class Blog {
+	private String blogTitle;
+	private String blogContent;
+	
+	public Blog(){}
+
+	public String getBlogTitle() {
+		return blogTitle;
+	}
+
+	public void setBlogTitle(String blogTitle) {
+		this.blogTitle = blogTitle;
+	}
+
+	public String getBlogContent() {
+		return blogContent;
+	}
+
+	public void setBlogContent(String blogContent) {
+		this.blogContent = blogContent;
+	}
+}
---
+++ youngeoun.Tis.0807/src/youngun/tis/travel/blog/BlogMapper.java
@@ -0,0 +1,5 @@
+package youngun.tis.travel.blog;
+
+public interface BlogMapper {
+	int insertBlog(Blog blog);
+}
