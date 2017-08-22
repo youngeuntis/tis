da82a9eddc41b82825aeb3968bbfdea13ba71357
205-19 <205-19@205_22>
2017-08-22 18:16:43 +0900

    성묵 java 추가

---
+++ youngeoun.Tis.0807/src/youngun/tis/mypage/domain/Personal.java
@@ -0,0 +1,66 @@
+package youngun.tis.mypage.domain;
+
+public class Personal {
+	private int memberNum;		//회원 번호
+	private String userId;		//회원 아이디
+	private String userPh;		//회원 핸드폰번호
+	private String userEmail;	//회원 이메일
+	private String userPw;		//회원 비밀번호
+	
+	public Personal() {}
+
+	public Personal(int memberNum, String userId, String userPh, String userEmail, String userPw) {
+		super();
+		this.memberNum = memberNum;
+		this.userId = userId;
+		this.userPh = userPh;
+		this.userEmail = userEmail;
+		this.userPw = userPw;
+	}
+
+	public int getmemberNum() {
+		return memberNum;
+	}
+
+	public void setmemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPh() {
+		return userPh;
+	}
+
+	public void setUserPh(String userPh) {
+		this.userPh = userPh;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	@Override
+	public String toString() {
+		return "Personal [memberNum=" + memberNum + ", userId=" + userId + ", userPh=" + userPh + ", userEmail=" + userEmail
+				+ ", userPw=" + userPw + "]";
+	}
+}
---
+++ youngeoun.Tis.0807/src/youngun/tis/mypage/domain/Profile.java
@@ -0,0 +1,57 @@
+package youngun.tis.mypage.domain;
+
+public class Profile {
+	private int memberNum;		//회원번호
+	private String userId;		//회원아이디
+	private String proflieImg;	//프로필사진
+	private String nickName;	//닉네임
+	
+	public Profile(){}
+
+	public Profile(int memberNum, String userId, String proflieImg, String nickName) {
+		super();
+		this.memberNum = memberNum;
+		this.userId = userId;
+		this.proflieImg = proflieImg;
+		this.nickName = nickName;
+	}
+
+	public int getmemberNum() {
+		return memberNum;
+	}
+
+	public void setmemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getProflieImg() {
+		return proflieImg;
+	}
+
+	public void setProflieImg(String proflieImg) {
+		this.proflieImg = proflieImg;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	@Override
+	public String toString() {
+		return "Profile [memberNum3=" + memberNum + ", userId=" + userId + ", proflieImg=" + proflieImg + ", nickName="
+				+ nickName + "]";
+	}
+
+}
