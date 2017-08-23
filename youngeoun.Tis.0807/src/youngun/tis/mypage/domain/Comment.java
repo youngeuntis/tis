04bebd5ef005a828121cff2430e01ca97a0de51f
205-20 <205-20@205-20-PC>
2017-08-23 13:55:16 +0900

    성묵 commentMore 작업

--- youngeoun.Tis.0807/src/youngun/tis/mypage/domain/Comment.java
+++ youngeoun.Tis.0807/src/youngun/tis/mypage/domain/Comment.java
@@ -1,5 +1,47 @@
 package youngun.tis.mypage.domain;
 
 public class Comment {
-	private Number userNum; //회원번호
+	private int memberNum; 			//회원번호
+	private String qnaReplyNum; 	//댓글번호
+	private String qnaReplyCont;	//댓글내용
+	
+	public Comment() {}
+
+	public Comment(int memberNum, String qnaReplyNum, String qnaReplyCont) {
+		super();
+		this.memberNum = memberNum;
+		this.qnaReplyNum = qnaReplyNum;
+		this.qnaReplyCont = qnaReplyCont;
+	}
+
+	public int getMemberNum() {
+		return memberNum;
+	}
+
+	public void setMemberNum(int memberNum) {
+		this.memberNum = memberNum;
+	}
+
+	public String getQnaReplyNum() {
+		return qnaReplyNum;
+	}
+
+	public void setQnaReplyNum(String qnaReplyNum) {
+		this.qnaReplyNum = qnaReplyNum;
+	}
+
+	public String getQnaReplyCont() {
+		return qnaReplyCont;
+	}
+
+	public void setQnaReplyCont(String qnaReplyCont) {
+		this.qnaReplyCont = qnaReplyCont;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [memberNum=" + memberNum + ", qnaReplyNum=" + qnaReplyNum + ", qnaReplyCont=" + qnaReplyCont
+				+ "]";
+	}
+	
 }
---
+++ youngeoun.Tis.0807/src/youngun/tis/mypage/service/CommentMoreServiceImpl.java
@@ -0,0 +1,16 @@
+package youngun.tis.mypage.service;
+
+import java.util.List;
+
+import youngun.tis.mypage.domain.Comment;
+
+public class CommentMoreServiceImpl implements CommentMoreService {
+
+	
+	
+	@Override
+	public List<Comment> findCommentMore(int memberNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
