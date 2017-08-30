e1506a074dcd4d1f62ffe193ede6f8e77a2fc6e4
205_07 <205_07@205_07-PC>
2017-08-30 14:16:05 +0900

    dz

--- youngeoun.Tis.0807/src/youngun/tis/review/dao/TravelDao.java
+++ youngeoun.Tis.0807/src/youngun/tis/review/dao/TravelDao.java
@@ -19,4 +19,8 @@ public class TravelDao implements TravelMapper {
 		return travelMapper.getCountryList(continentCode);
 	}
 	
+	@Override
+	public Country searchParam(String nationCode) {
+		return travelMapper.searchParam(nationCode);
+	}
 }
